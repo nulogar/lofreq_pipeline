59539b2a37530aea99c8ac055562126181097d8a
nulogar <nulogar@gmail.com>
2018-08-23 13:20:56 +0200

    cambios a histogramLofreq.R para generalizarlo

--- histogramLofreq.R
+++ histogramLofreq.R
@@ -8,6 +8,6 @@ muestra <- read_tsv(file=args[1], col_types=cols(POS=col_character()))
 
 pdf( paste(args[2],".pdf",sep="") )
 
-ggplot(data=muestra, aes(muestra$`ALLELE FREQUENCY`)) + geom_histogram() +ylim(0,600) + labs(title=args[2], x="Allele frequency", y="count")
+ggplot(data=muestra, aes(muestra$`ALLELE FREQUENCY`)) + geom_histogram() + labs(title=args[2], x="Allele frequency", y="count")
 
 dev.off()
